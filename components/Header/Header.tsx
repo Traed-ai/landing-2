be75d84ed263374e1dfd869e3fff737b1c309e76
Pranjal Varshney <pranjal25varshney@gmail.com>
2023-09-24 21:54:49 +0530

    header working

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,19 @@
+"use client";
 import React from "react";
 import styles from "./header.module.css";
 import Container from "../Container/Container";
 import Image from "next/image";
 import Button from "../Button/Button";
+import { usePathname, useRouter } from "next/navigation";
 
 function Header() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isActive = (href: string) => {
+    return pathname === href ? styles.headerNavItemActive : "";
+  };
+
   return (
     <div className={styles.headerWrapper}>
       <Container>
@@ -18,11 +27,35 @@ function Header() {
             />
           </div>
           <div className={styles.headerNav}>
-            <div className={styles.headerNavItem}>About</div>
-            <div className={styles.headerNavItem}>Our Solutions</div>
-            <div className={styles.headerNavItem}>Sectors</div>
-            <div className={styles.headerNavItem}>Knowledge</div>
-            <div className={styles.headerNavItem}>Company</div>
+            <div
+              onClick={() => router.push("/")}
+              className={`${styles.headerNavItem} ${isActive("/")}`}
+            >
+              Home
+            </div>
+            <div
+              onClick={() => router.push("/about")}
+              className={`${styles.headerNavItem} ${isActive("/about")}`}
+            >
+              About
+            </div>
+            <div
+              onClick={() => router.push("/solutions")}
+              className={`${styles.headerNavItem} ${isActive("/solutions")}`}
+            >
+              Our Solutions
+            </div>
+            {/* <div onClick={()=>router.push('/')} className={`${styles.headerNavItem} ${isActive("/sectors")}`}>
+              Sectors
+            </div>
+            <div
+              className={`${styles.headerNavItem} ${isActive("/knowledge")}`}
+            >
+              Knowledge
+            </div> */}
+            {/* <div onClick={()=>router.push('/')} className={`${styles.headerNavItem} ${isActive("/company")}`}>
+              Company
+            </div> */}
             <Button
               borderRadius="6px"
               color="#fff"
