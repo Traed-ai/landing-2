28546baaa56e20c0ea6e2306406a293f26d3eeac
Pranjal Varshney <pranjal25varshney@gmail.com>
2023-10-02 21:05:40 +0530

    fixes added

--- components/Home/TopBanner/TopBanner.tsx
+++ components/Home/TopBanner/TopBanner.tsx
@@ -1,15 +1,17 @@
-"use client"
-import { Button, Grid, Typography } from "@mui/material";
+"use client";
+import { Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 import topBan from "@/public/assets/topBan.jpg";
 import "./banner.scss";
 import Container from "@/components/Container/Container";
-import useWindowDimensions from "../../../hooks/useWindowDimensions"
+import useWindowDimensions from "../../../hooks/useWindowDimensions";
+import Link from "next/link";
+import Button from "@/components/Button/Button";
 
 const TopBanner = () => {
-  const {width} = useWindowDimensions()
- 
+  const { width } = useWindowDimensions();
+
   return (
     <Grid
       sx={{
@@ -19,39 +21,54 @@ const TopBanner = () => {
       className="BannerImg"
     >
       <Grid className="imgGrid">
-  
+        <div
+          className={"imgBg"}
+          style={{
+            width: "100%",
+            height: width && width > 768 ? "900px" : "500px",
+          }}
+        />
         <Image
           style={{
             width: "100%",
-            height: "100%",
+            height: width && width > 768 ? "900px" : "500px",
+            objectFit: "cover",
+            objectPosition: "right center",
           }}
           src={topBan}
           alt=""
         />
       </Grid>
       <Container>
-        <Grid className="textDiv">
-          <Typography className="bannerHeading">
+        <div className="textDiv">
+          <div className="bannerHeading">
             Accounting, but make it easy - withTraed!
-          </Typography>
-          <Typography className="bannerText">
+          </div>
+          <div className="bannerText">
             Applications that power financial institutions, marketplaces that
             accelerate industry & an open innovation platform for banks,
             fintechs & non-banks to connect and collaborate.
-          </Typography>
-          <button style={{
-            background:"#06BA63",
-            borderColor:"transparent",
-            borderRadius: "6px",
-            boxShadow:"0px 1px 4px 0px rgba(25, 33, 61, 0.08)",
-            padding:"14px 18px;",
-            textAlign:"center",
-            fontSize:"14px",
-            fontWeight:"600",
-            color:"#fff",
-            marginTop:"15px"
-          }} >Get Started</button>
-        </Grid>
+          </div>
+          <Link
+            href={"https://portal.traed.ai"}
+            passHref
+            style={{
+              textDecoration: "none",
+              color: "#000",
+              // margin: "0 auto",
+            }}
+          >
+            <Button
+              borderRadius="6px"
+              color="#fff"
+              width="130px"
+              height="40px"
+              backgroundColor="#06BA63"
+            >
+              Get Started
+            </Button>
+          </Link>
+        </div>
       </Container>
     </Grid>
   );
