d881231a0cdcec6d8498e2326ec0116717cc2e5f
adityamanas <aditya.manas01@gmail.com>
2023-10-02 17:51:53 +0530

    ui fix

--- components/Home/TopBanner/TopBanner.tsx
+++ components/Home/TopBanner/TopBanner.tsx
@@ -1,10 +1,15 @@
-import { Grid } from "@mui/material";
+"use client"
+import { Button, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 import topBan from "@/public/assets/topBan.jpg";
 import "./banner.scss";
+import Container from "@/components/Container/Container";
+import useWindowDimensions from "../../../hooks/useWindowDimensions"
 
 const TopBanner = () => {
+  const {width} = useWindowDimensions()
+ 
   return (
     <Grid
       sx={{
@@ -14,6 +19,7 @@ const TopBanner = () => {
       className="BannerImg"
     >
       <Grid className="imgGrid">
+  
         <Image
           style={{
             width: "100%",
@@ -23,9 +29,30 @@ const TopBanner = () => {
           alt=""
         />
       </Grid>
-      <Grid className="textDiv" >
-
-      </Grid>
+      <Container>
+        <Grid className="textDiv">
+          <Typography className="bannerHeading">
+            Accounting, but make it easy - withTraed!
+          </Typography>
+          <Typography className="bannerText">
+            Applications that power financial institutions, marketplaces that
+            accelerate industry & an open innovation platform for banks,
+            fintechs & non-banks to connect and collaborate.
+          </Typography>
+          <button style={{
+            background:"#06BA63",
+            borderColor:"transparent",
+            borderRadius: "6px",
+            boxShadow:"0px 1px 4px 0px rgba(25, 33, 61, 0.08)",
+            padding:"14px 18px;",
+            textAlign:"center",
+            fontSize:"14px",
+            fontWeight:"600",
+            color:"#fff",
+            marginTop:"15px"
+          }} >Get Started</button>
+        </Grid>
+      </Container>
     </Grid>
   );
 };
